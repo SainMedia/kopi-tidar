a276dc7eea8597e6611029ab8adbfa0ac5319cfb
SainMedia <mhaytb@gmail.com>
2026-06-04 22:45:40 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,5 @@
-const CACHE_NAME = 'kopi-tidar-v1';
-const ASSETS = ['./','./index.html','./manifest.json','./icon.svg'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
-});
+const CACHE_NAME='kopi-tidar-v3';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./service-worker.js'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
